db985b5d36f834c40ce56cfb2b6daf3f2ec67f53
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-14 14:45:52 +0300

    adding functionality

--- library.js
+++ library.js
@@ -2,11 +2,11 @@
 let myLibrary = [];
 
 //book cobject
-function Book(title, author, pages, isread=false) 
+function Book(title, author, page, isread=false) 
 {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.page = page;
     this.isread = isread;
 }
 
@@ -30,5 +30,45 @@ function removeBookfromLibrary(book)
 const addbtn = document.querySelector('#add-book')
 addbtn.addEventListener('click', () =>
 {
-    console.log("test")
+    openForm()
 });
+//form cancel button
+const cancelbtn = document.querySelector('.cancel-button')
+cancelbtn.addEventListener('click', () =>
+{
+    closeForm()
+});
+
+//add book button
+let addform = document.getElementById('add-book-form');
+addform.onsubmit = () => {
+    //let ftitle = document.getElementById('ftitle').value;
+    //let fauthor = document.getElementById('fauthor').value;
+    //let fpage = document.getElementById('fpage').value;
+    //let isread = document.getElementById('fisread').value;
+
+    //let new_book = new Book(ftitle, fauthor, fpage, isread)
+    //console.log(new_book)
+
+    //let temp = getFromStorage("stored_classes")
+    //temp.push(new_class)
+    //removeFromStorage("stored_classes")
+    //addToStorage("stored_classes" ,temp)
+    console.log(ftitle = document.getElementById('ftitle'))
+    return false;
+};
+
+
+//open addbook form
+function openForm()
+{
+    document.getElementById("form-popup").style.display = "block";
+}
+
+//close addbook form
+function closeForm()
+{
+    document.getElementById("form-popup").style.display = "none";
+}
+
+openForm()
